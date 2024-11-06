a6479295ca3d4fe730abb9656f20a9db8fd2f8a5
Bohdan Razhyk <>
2024-11-06 11:40:29 +0100

    completed code for class Birthday

--- src/constants.py
+++ src/constants.py
@@ -3,3 +3,5 @@
 PHONE_REGEXP = re.compile(r"^\d{10}$")
 
 EMAIL_REGEXP = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+
+DATE_REGEXP = re.compile(r"^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})$")
