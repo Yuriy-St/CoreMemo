42bab6270b5622069b2f4f8ff5e2993c0bd5f82a
Bohdan Razhyk <>
2024-11-06 10:01:30 +0100

    created class Note, class Title, class Description with simple validation

---
+++ src/note.py
@@ -0,0 +1,11 @@
+﻿from src.description import Description
+from src.title import Title
+
+
+class Note:
+    def __init__(self, title: str, description: str):
+        self.title = Title(title)
+        self.description = Description(description)
+
+    def __str__(self):
+        return f"Title: {self.title.value}, description: {self.description.value}"
