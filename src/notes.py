4f186d6a20786b750e2b2b56517e6234fd63bf18
Serezon <serezon@protonmail.com>
2024-11-07 19:45:13 +0200

    feature: add cli commands for notes

--- src/notes.py
+++ src/notes.py
@@ -1,6 +1,8 @@
 from collections import UserDict
-from note import Note
-from custom_exceptions import InputException
+from .note import Note
+from .title import Title
+from .description import Description
+from .custom_exceptions import RecordNotFountException
 
 
 class Notes(UserDict[int, Note]):
@@ -16,7 +18,7 @@ class Notes(UserDict[int, Note]):
 
     @staticmethod
     def notes_with_id(notes: dict[int, Note]) -> str:
-        return "\n".join([f"id = {id}: {note}" for id, note in notes.items()])
+        return "\n".join([f"id = {id} - {note}" for id, note in notes.items()])
 
     def __str__(self) -> str:
         return self.notes_with_id(self.data)
@@ -26,7 +28,7 @@ class Notes(UserDict[int, Note]):
         self.id += 1
 
     def _search_by(self, key: str, query: str) -> dict[int, Note]:
-        return { id: note for id, note in self.data.items() if note[key].lower().find(query.lower()) != -1 }
+        return { id: note for id, note in self.data.items() if getattr(note, key).value.lower().find(query.lower()) != -1 }
 
     def search_by_title(self, title: str) -> dict[int, Note]:
         return self._search_by("title", title)
@@ -36,41 +38,18 @@ class Notes(UserDict[int, Note]):
         
     def update_title(self, id: int, title: str):
         if not self._note_exists(id):
-            raise InputException("Note with this id does not exist")
+            raise RecordNotFountException("Note with this id does not exist")
         
-        self.data[id].title = title
+        self.data[id].title = Title(title)
 
     def update_description(self, id: int, description: str):
         if not self._note_exists(id):
-            raise InputException("Note with this id does not exist")
+            raise RecordNotFountException("Note with this id does not exist")
 
-        self.data[id].description = description
+        self.data[id].description = Description(description)
 
     def remove(self, id: int):
         if not self._note_exists(id):
-            raise InputException("Note with this id does not exist")
+            raise RecordNotFountException("Note with this id does not exist")
 
         del self.data[id]
-
-if __name__ == "__main__":
-    notes = Notes()
-
-    dictActions = [
-        { "method": notes.add, "values": [ Note("Title 1", "Description 1") ] },
-        { "method": notes.add, "values": [ Note("Title 2", "Description 2") ] },
-        { "method": notes.add, "values": [ Note("Title 3", "Description 3") ] },
-        { "method": notes.remove, "values": [ 1 ] },
-        { "method": notes.update_title, "values": [ 2, "New title" ] },
-        { "method": notes.update_description, "values": [ 2, "New description" ] },
-        { "method": notes.search_by_title, "values": [ "New title" ] },
-        { "method": notes.search_by_description, "values": [ "New description" ] },
-    ]
-    
-    for item in dictActions:
-        try:
-            values = item["values"]
-            item["method"].__call__(*values)
-
-            print(notes, "\n")
-        except Exception as e:
-            print(f"Values: {values}, error: {e}")
