34360600d6973f858d5cfc36b78785fdcfa71066
Yuriy-St <yuriystaynov@gmail.com>
2024-11-09 02:21:28 +0200

    fix: updating input_error messages

--- src/decorators.py
+++ src/decorators.py
@@ -3,20 +3,19 @@ from typing import Callable
 from .custom_exceptions import InputException, RecordNotFountException
 
 
-# TODO: to be updated for each exception type
 def input_error(func: Callable) -> Callable:
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except (InputException, RecordNotFountException) as e:
-            return f"{e}"
+            return f"{e}" if e.args else "Please, input data correctly"
         except ValueError as e:
-            return f"{e}"
+            return f"{e}" if e.args else "Incorrect value!"
         except KeyError as e:
-            return f"{e}"
+            return f"{e}" if e.args else "Item not found"
         except IndexError as e:
-            return f"{e}"
+            return f"{e}" if e.args else "Incorrect number of arguments"
         except Exception as e:
-            return f"{e}"
+            return f"{e}" if e.args else "Oops! Something went wrong..."
 
     return inner
